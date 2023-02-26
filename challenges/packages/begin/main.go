24a38a19cea55f22a90d774fe4788712d6f0c8ea
Zaid Aziz <67843799+zaidazaize@users.noreply.github.com>
2023-02-26 16:09:09 +0000

    2. basics

--- challenges/packages/begin/main.go
+++ challenges/packages/begin/main.go
@@ -1,10 +1,19 @@
 // challenges/packages/begin/proverbs.go
 package main
 
+import (
+	"fmt"
+
+	"github.com/jboursiquot/go-proverbs"
+)
+
 // import the proverbs package
 
 // getRandomProverb returns a random proverb from the proverbs package
-
+func getRandomProverb()string{
+	return proverbs.Random().Saying
+}
 func main() {
 	// print the result of calling your getRandomProverb function
+	fmt.Println(getRandomProverb())
 }
--- functions/begin/main.go
+++ functions/begin/main.go
@@ -1,18 +1,37 @@
 // functions/begin/main.go
 package main
 
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
+
 // simple greet function
 //
-
+func greet() string {
+	return "Hello"
+}
 // greetWithName returns a greeting with the name
-//
+func greetWithName(name string) string{
+	return "Hellw " + name + "!"
+}
 
 // greetWithName returns a greeting with the name and age of the person
 //
-
+func greetWithNameAndAge(name  string, age int)(greeting string){
+	greeting =  "hello " + name + "I am of "+ strconv.Itoa(age) + " years old."
+	return 
+}
 // divide divides two numbers and returns the result
 // if the second number is zero, it returns  error
 //
+func divide(a,b int)(int ,error){
+	if b==0 {
+		return 0, errors.New("divide by zero")
+	}
+	return a/b, nil
+}
 
 func main() {
 	// invoke greet function
@@ -20,10 +39,10 @@ func main() {
 
 	// invoke greetWithName function
 	// fmt.Println(greetWithName("Toni"))
-
+	// fmt.Println(greetWithNameAndAge("Toni ",5))
 	// invoke divide function
-	// fmt.Println(divide(10, 2))
+	fmt.Println(divide(10, 2))
 
 	// invoke divide function with zero denominator to get an error
-	// fmt.Println(divide(5, 0))
+	fmt.Println(divide(5, 0))
 }
